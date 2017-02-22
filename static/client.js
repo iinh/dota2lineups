b4c71908fb25f4a272d0f037e623c7597fba434f
Niklas Hätty <nikha864@student.liu.se>
2017-02-22 23:24:29 +0100

    Remove console logs

--- static/client.js
+++ static/client.js
@@ -27,8 +27,6 @@ function fillMatches(sorting){
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             res = JSON.parse(this.responseText);
-            console.log(res.success);
-            console.log(res);
 
             if (res.success){
                 for (var i = 0; i < matchesShown; i++){
