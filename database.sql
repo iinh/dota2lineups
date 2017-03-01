e41c374d671b9aeed49f12385a595cc1bb40869c
Niklas Hätty <nikha864@student.liu.se>
2017-03-01 18:03:56 +0100

    fix a smarter way to do top tables

--- database.sql
+++ database.sql
@@ -15,7 +15,7 @@ create table if not exists match_ids(
     foreign key(lineup_key) references lineups(lineup_key)
 );
 
-create table if not exists top_wins(
+create table if not exists top_lineups(
     lineup_key varchar(20) not null unique,
     wins integer not null default 0,
     losses integer not null default 0,
@@ -27,47 +27,4 @@ create table if not exists top_wins(
     foreign key(losses) references lineups(losses),
     foreign key(win_rate) references lineups(win_rate),
     foreign key(win_rate) references lineups(weighted_sort)
-)
-
-create table if not exists top_win_rate(
-    lineup_key varchar(20) not null unique,
-    wins integer not null default 0,
-    losses integer not null default 0,
-    win_rate real not null,
-    weighted_sort real not null,
-    primary key(lineup_key),
-    foreign key(lineup_key) references lineups(lineup_key),
-    foreign key(wins) references lineups(wins),
-    foreign key(losses) references lineups(losses),
-    foreign key(win_rate) references lineups(win_rate),
-    foreign key(win_rate) references lineups(weighted_sort)
-)
-
-create table if not exists top_weighted_sort(
-    lineup_key varchar(20) not null unique,
-    wins integer not null default 0,
-    losses integer not null default 0,
-    win_rate real not null,
-    weighted_sort real not null,
-    primary key(lineup_key),
-    foreign key(lineup_key) references lineups(lineup_key),
-    foreign key(wins) references lineups(wins),
-    foreign key(losses) references lineups(losses),
-    foreign key(win_rate) references lineups(win_rate),
-    foreign key(win_rate) references lineups(weighted_sort)
-)
-
-create table if not exists top_losses(
-    lineup_key varchar(20) not null unique,
-    wins integer not null default 0,
-    losses integer not null default 0,
-    win_rate real not null,
-    weighted_sort real not null,
-    primary key(lineup_key),
-    foreign key(lineup_key) references lineups(lineup_key),
-    foreign key(wins) references lineups(wins),
-    foreign key(losses) references lineups(losses),
-    foreign key(win_rate) references lineups(win_rate),
-    foreign key(win_rate) references lineups(weighted_sort)
-)
-
+);
