4ef324d4cd6f78566ec8bfd94bba42f10dedfd9a
Niklas Hätty <nikha864@student.liu.se>
2017-03-01 17:54:12 +0100

    Add support for top 100 tables

--- database.sql
+++ database.sql
@@ -1,7 +1,4 @@
-drop table if exists match_ids;
-drop table if exists lineups;
-
-create table lineups(
+create table if not exists lineups(
     lineup_key varchar(20) not null unique,
     wins integer not null default 0,
     losses integer not null default 0,
@@ -10,10 +7,67 @@ create table lineups(
     primary key(lineup_key)
 );
 
-create table match_ids(
+create table if not exists match_ids(
     lineup_key varchar(20) not null,
     match_id bigint not null,
     win boolean not null,
     primary key(lineup_key, match_id),
     foreign key(lineup_key) references lineups(lineup_key)
 );
+
+create table if not exists top_wins(
+    lineup_key varchar(20) not null unique,
+    wins integer not null default 0,
+    losses integer not null default 0,
+    win_rate real not null,
+    weighted_sort real not null,
+    primary key(lineup_key),
+    foreign key(lineup_key) references lineups(lineup_key),
+    foreign key(wins) references lineups(wins),
+    foreign key(losses) references lineups(losses),
+    foreign key(win_rate) references lineups(win_rate),
+    foreign key(win_rate) references lineups(weighted_sort)
+)
+
+create table if not exists top_win_rate(
+    lineup_key varchar(20) not null unique,
+    wins integer not null default 0,
+    losses integer not null default 0,
+    win_rate real not null,
+    weighted_sort real not null,
+    primary key(lineup_key),
+    foreign key(lineup_key) references lineups(lineup_key),
+    foreign key(wins) references lineups(wins),
+    foreign key(losses) references lineups(losses),
+    foreign key(win_rate) references lineups(win_rate),
+    foreign key(win_rate) references lineups(weighted_sort)
+)
+
+create table if not exists top_weighted_sort(
+    lineup_key varchar(20) not null unique,
+    wins integer not null default 0,
+    losses integer not null default 0,
+    win_rate real not null,
+    weighted_sort real not null,
+    primary key(lineup_key),
+    foreign key(lineup_key) references lineups(lineup_key),
+    foreign key(wins) references lineups(wins),
+    foreign key(losses) references lineups(losses),
+    foreign key(win_rate) references lineups(win_rate),
+    foreign key(win_rate) references lineups(weighted_sort)
+)
+
+create table if not exists top_losses(
+    lineup_key varchar(20) not null unique,
+    wins integer not null default 0,
+    losses integer not null default 0,
+    win_rate real not null,
+    weighted_sort real not null,
+    primary key(lineup_key),
+    foreign key(lineup_key) references lineups(lineup_key),
+    foreign key(wins) references lineups(wins),
+    foreign key(losses) references lineups(losses),
+    foreign key(win_rate) references lineups(win_rate),
+    foreign key(win_rate) references lineups(weighted_sort)
+)
+
